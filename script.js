ce25d196e0aaf77348ab86289c1207d225505e23
gaurav <139613687+ergaurav3155@users.noreply.github.com>
2024-08-23 14:54:49 +0530

    add read file

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 
-fs.writeFile("gaurav.txt","hellow gaurav",function(err){
-    if(err) console.log(err);
-    else console.log("file created");
+fs.writeFile("gaurav.txt", "hello hellow gaurav", function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("File created");
+    }
+});
 
+fs.readFile("gaurav.txt","utf8",function(err,data){
+    if(err)console.log(err);
+    else console.log(data);
 })
